0e5ed51d6ecf45a44562e401a816937c961b68b3
NandoRoG <ferrodgonzalezx1000@gmail.com>
2022-07-04 04:16:49 -0400

    Integrar

--- ExpressionCalculator/IntegralesDefinidas.cs
+++ ExpressionCalculator/IntegralesDefinidas.cs
@@ -1,17 +1,18 @@
 namespace ExpressionCalculator
 {
-    public class IntegralDefinida
+    public static class IntegralDefinida
     {
-        public double Integra(Expression exp,int sup,int inf)
+        public static double Integra(Expression exp,int inf,int sup)
         {
             Expression answer=new ConstantOrVariable("0");
             Dictionary<char,double> aux=new Dictionary<char,double>();
             aux.Add('x',inf);
-            for(int i=inf;i<=sup;i=i+1/1000)
+            for(double i=inf;i<=sup;i=i+(double)1/(double)1000)
             {
                 aux['x']=i;
-                answer= new Sum("+",exp.Evaluate(aux),answer);
+                answer= new Sum("+",exp,answer).Evaluate(aux);
             }
+            answer = new Divide("/", answer, new ConstantOrVariable("1000")).Evaluate(new Dictionary<char, double>());
             return Double.Parse(answer.ToString());
         }
        
